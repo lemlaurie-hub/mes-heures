6243eb5f36a87faf18e105a12628cd06caca3f9b
lemlaurie-hub <lemlaurie@gmail.com>
2026-09-14 20:24:29 +0200

    Refresh cache after week-number fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mes-heures-v36-clean';
+const CACHE='mes-heures-v37-week-number';
 const ASSETS=['./','./index.html','./styles.css','./core.js','./domain.js','./v17.js','./ui.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
